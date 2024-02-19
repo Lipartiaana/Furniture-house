d7cded6070cca6fa072fa63aa96f27d444c476e7
Lipartiaana <124500573+Lipartiaana@users.noreply.github.com>
2024-02-19 21:05:54 +0400

    Adding 'discount' section on homepage.

--- JS/script.js
+++ JS/script.js
@@ -9,7 +9,7 @@ function createFeatureHtml(feature) {
             </div>`;
 }
 
-fetch("/data.json")
+fetch("./data.json")
   .then((response) => response.json())
   .then((data) => {
     const swiperContainer = document.querySelector(".swiper-wrapper");
